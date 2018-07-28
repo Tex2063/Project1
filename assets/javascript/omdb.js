fb9dc12971302aa88da38338f94d95913a8d99fb
Kaan <Kaan@Kaans-Mac.localdomain>
2018-07-28 12:34:22 -0700

    updated omdb

--- assets/javascript/omdb.js
+++ assets/javascript/omdb.js
@@ -6,12 +6,12 @@ $(document).ready(function () {
 
   // return API link with searched item
   function getUrlOmdb(search) {
-    
+
     return "https://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=9170740"
-    
+
 
   }
-//Establish the API connection and get values.
+  //Establish the API connection and get values.
   function callOmdb(search) {
     $.ajax({
       url: getUrlOmdb(search),
@@ -20,8 +20,8 @@ $(document).ready(function () {
       handleResponse(response);
 
 
-  //fire if there is API connection issue
-     })
+      //fire if there is API connection issue
+    })
     //  .error(function (request, response, error) {
     //   console.log(error);
     // })
@@ -29,99 +29,46 @@ $(document).ready(function () {
   }
 
 
-  //function getInput(input) {
+  function getInput(input) {
 
     $("#theButton").on("click", function (event) {
       // event.preventDefault();
       input = $("#movie-input").val().trim();
-      if (input ==""){
-        alert("please enter a movie name"); 
-    }
-    else{
-      //console.log(input);
-      callOmdb(input);
-      //console.log(getUrlOmdb(input)); 
-    }
+      if (input == "") {
+        alert("please enter a movie name");
+      }
+      else {
+        //console.log(input);
+        callOmdb(input);
+        //console.log(getUrlOmdb(input)); 
+      }
 
     });
-    // return input; 
-  //}
+  }
 
   function handleResponse(response) {
-//console.log(response);
-    var rottenDiv; 
+    //console.log(response);
     var poster = response.Poster;
-    var moviePlot=response.Plot;
-    var movieName =response.Title;
-    var rating = response.Ratings[0].Value; 
-    
+    var moviePlot = response.Plot;
+    var movieName = response.Title;
+    var rating = response.Ratings[0].Value;
+
 
     //append movie poster
-    $(".moviePoster").html("<img src=" + poster +">"+"</img>");
+    $(".moviePoster").html("<img src=" + poster + ">" + "</img>");
 
 
     //add movie title
-    $("#movieName").text(movieName);
+    $(".movieName").text(movieName);
 
     //add movie plot 
     $(".moviePlot").text(moviePlot);
 
-    $(".movieRating").text("Ratings: "+ rating);
+    $(".movieRating").text("Ratings: " + rating);
     console.log(rating);
 
   }
 
-//getInput(); 
+  getInput();
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /*// OMDB SCRIPT THINGS
-    // variables
-var movie = "";
-var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-
-var createRow = function(data) {
-    // Get reference to existing tbody element, create a new table row element
-    var tBody = $("tbody");
-    var tRow = $("<tr>");
-
-    // Methods run on jQuery selectors return the selector they we run on
-    // This is why we can create and save a reference to a td in the same statement we update its text
-    var titleTd = $("<td>").text(data.Title);
-    var plotTd = $("<td>").text(data.Plot);
-    var ratingTd = $("<td>").text(data.Rating);
-    // Append the newly created table data to the table row
-    tRow.append(titleTd, plotTd, ratingTd);
-    // Append the table row to the table body
-    tBody.append(tRow);
-  };
-
-  // The search OMDB function takes a movie, searches the omdb api for it, and then passes the data to createRow
-  var searchOMDB = function(movie) {
-    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      createRow(response);
-    });
-  };
-
-  $("#button").on("click", function() {
-    alert("I've been clicked!");
-  });
-  */
-
